6bbb18087ddb40f5a58619d9e975f98d6b4e9e70
tprentis <tprentis@gmail.com>
2014-03-21 15:55:16 -0400

    more PW Reset Stuff

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,7 +17,7 @@ class PasswordResetsController < ApplicationController
 
     if @member.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Password reset has expired."
-    elsif @member.update_attributes(password_reset_params) #params[:member])
+    elsif @member.update_attributes(params[:member])
       redirect_to root_url, :notice => "Password has been reset."
     else
       render :edit
