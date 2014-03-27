010936cd604ec40ef0fad9e94e79a792a5cfdf86
tprentis <tprentis@gmail.com>
2014-03-27 15:22:54 -0400

    &rarr in _job.html.erb

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,8 @@
 module JobsHelper
+
+def add_asset_link(name)
+    link_to_function name do |job|
+      job.insert_html :bottom, :assets, :partial => 'asset', :object => Asset.new
+    end
+  end
 end
